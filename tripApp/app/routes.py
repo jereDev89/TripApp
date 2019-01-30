6da05ef622f422608698e5d91726132e554a67b5
JereDesign <33057840+JereDesign@users.noreply.github.com>
2019-01-30 09:44:22 +0100

    srijeda+

--- tripApp/app/routes.py
+++ tripApp/app/routes.py
@@ -8,14 +8,26 @@ from flask_login import login_required
 
 
 @app.route('/')
+@app.route('/index' , methods=['GET', 'POST'])
+
+
+
 @app.route('/index', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profil'))
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('index'))
-        return redirect('profil')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('profil'))
+    return render_template('index.html', form=form)
+
+
+
 
 
 @app.route('/logout')
