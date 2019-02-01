eeb4e2206868a9588af8b661c13838febc307a0d
JereDesign <33057840+JereDesign@users.noreply.github.com>
2019-02-01 09:53:26 +0100

    jere  -  petak

--- tripApp/app/forms.py
+++ tripApp/app/forms.py
@@ -1,9 +1,43 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField, DateField, DecimalField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('login')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    ime_korisnika = StringField('Ime', validators=[DataRequired()])
+    email = StringField('Email' , validators=[DataRequired()])
+    mjesto_stanovanja = StringField('Mjesto stanovanja')
+    username = StringField('Username' , validators=[DataRequired()])
+    password = PasswordField('Password' , validators=[DataRequired()])
+    # password_repeat = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password_hash')])
+    submit = SubmitField('Registracija')
+
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different username.')
+    #
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Please use a different email address.')
+
+
+class CreateTripForm(FlaskForm):
+    naziv_izleta = StringField('Naziv_izleta', validators=[DataRequired()])
+    lokacija = StringField('Lokacija', validators=[DataRequired()])
+    datum = DateField('Datum', validators=[DataRequired()])
+    minimalan = StringField('Min_broj_sudionika', validators=[DataRequired()])
+    maksimalan = StringField('Max_broj_sudionika', validators=[DataRequired()])
+    prijevoz = StringField('Prijevoz', validators=[DataRequired()])
+    opis = StringField('Opis_izleta', validators=[DataRequired()])
+    cijena = DecimalField('Cijena', validators=[DataRequired()])
+    dodaj = SubmitField('dodaj_izlet')
--- tripApp/app/routes.py
+++ tripApp/app/routes.py
@@ -1,8 +1,7 @@
-from app import app
+from app import app , db
 from flask import render_template, url_for, flash, redirect
-from app.forms import LoginForm
-from flask_login import current_user, login_user
-from app.models import User
+from app.forms import LoginForm , RegisterForm , CreateTripForm
+from app.models import User, Izlet , Kombinacija
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -19,7 +18,7 @@ def login():
             return redirect(url_for('index'))
         login_user(user)
         return redirect(url_for('profil', username=current_user.username))
-    return render_template('index.html', title='Sign In', form=form)
+    return render_template('index.html', form=form)
 
 
 @app.route('/logout')
@@ -28,15 +27,16 @@ def logout():
     return redirect(url_for('login'))
 
 
-
-@app.route('/registracija')
+@app.route('/registracija' , methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        user = User(ime_korisnika=form.ime.data, )
+        user = User(ime_korisnika=form.ime_korisnika.data, email=form.email.data, mjesto_stanovanja=form.mjesto_stanovanja.data, username=form.username.data)
+        user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('index'))
     return render_template('registracija.html', form=form)
 
 
@@ -46,10 +46,15 @@ def profil(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('profil.html', user=user)
 
-    return render_template('profil.html')
-
 
 @app.route('/dodajIzlet')
 def dodajIzlet():
-
     return render_template('dodavanje_izleta.html')
+
+@app.route('/izleti')
+def izleti():
+    return render_template('izlet.html')
+
+@app.route('/popisIzleta')
+def popisIzleta():
+    return render_template('popis_izleta.html')
